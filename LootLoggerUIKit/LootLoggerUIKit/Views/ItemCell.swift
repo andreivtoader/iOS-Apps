144fc1b46e6f75e4278e12f30b80e05b24157ab3
Andrei Toader <andreivtoader@gmail.com>
2023-04-21 17:18:40 +0300

    Added LootLogger app for UIKit.

---
+++ LootLoggerUIKit/LootLoggerUIKit/Views/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  LootLoggerUIKit
+//
+//  Created by Andrei Toader on 21.04.2023.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    
+}
